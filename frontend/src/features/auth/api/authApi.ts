eb744d7121fa7b09296c87db0a751ec55c4b1ff6
Elena <ek28150@gmail.com>
2026-05-15 19:59:19 -0700

    installed font icons, modified App.tsx with Protected route and Login abd Register routes, created authentication

---
+++ frontend/src/features/auth/api/authApi.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+});
+
+export const registerUser = async (name:string, email: string, password:string) => {
+  try {
+    const {data} = await api.post(
+      "/auth/register",
+      {name, email, password}
+    ) 
+    return  data;
+  } catch (error: any) {
+      const message = error?.response?.data?.message || "Register failed";
+    throw new Error (message)
+  }
+}
+
+export const loginUser = async (email: string, password:string) => {
+  try {
+    const {data} = await api.post(
+      "auth/login",
+      {email, password}
+    ) 
+    return  data;
+  } catch (error: any) {
+      const message = error?.response?.data?.message || "Login failed";
+    throw new Error (message)
+  }
+}
+
+
---
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../context/useAuth";
+import styles from "./RegisterForm.module.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+// 	faEye,
+// 	faEyeSlash,
+// } from "@fortawesome/free-solid-svg-icons";
+
+type FormState = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export default function RegisterForm() {
+  const [form, setForm] = useState<FormState>({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const {register} = useAuth()
+  const navigate = useNavigate()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (form.password !== form.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+    await register(form.name, form.email, form.password);
+    navigate("/login"); 
+  } catch (err: any) {
+    console.log(err.message);
+  }
+  };
+
+  return (
+    <div className={styles.panel}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Create Account</h1>
+        <p className={styles.subtitle}>Start your adventure today!</p>
+
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            className={styles.input}
+            name="username"
+            placeholder="Username"
+            value={form.name}
+            onChange={handleChange}
+          />
+
+          <input
+            className={styles.input}
+            name="email"
+            placeholder="Email"
+            type="email"
+            value={form.email}
+            onChange={handleChange}
+          />
+
+          <input
+            className={styles.input}
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={form.password}
+            onChange={handleChange}
+          />
+
+          <input
+            className={styles.input}
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            type="password"
+            value={form.confirmPassword}
+            onChange={handleChange}
+          />
+
+          <button className={styles.button} type="submit">
+            Register
+          </button>
+          <div className={styles.divider}>OR</div>
+
+				<button type="button" className={styles.footerButton}>
+					Already have an account? <span>Log In</span>
+				</button>
+        </form>
+      
+      </div>
+    </div>
+  );
+}
