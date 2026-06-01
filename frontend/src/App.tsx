09abd704ee25b27ccc9c6ffbe1af83b44262e754
Raul Tejera <rtejera.w@gmail.com>
2026-06-01 15:04:01 -0500

    Fixing some Auth details
    - 'services/api.ts': Functions are added to handle and pass the authentication token during API calls.
    - 'ProtectedRoute': Modified to redirect to 'login'—rather than 'register'—when a user is unauthenticated while attempting to access a protected route.
    - 'AuthContext': An 'isInitialized' state is added to prevent rendering child components during the initial load, before the presence of an authentication token has been verified. This prevents rendering issues and errors in scenarios where a token exists and the page is subsequently reloaded.
    - 'app.tsx': The 'AuthProvider' is added as the parent context at the root of the component hierarchy.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,70 +15,81 @@ import DungeonPage from "./features/Dungeon/pages/dungeonPage";
 import MarketplacePage from "./features/Marketplace/pages/MarketplacePage";
 
 import ProtectedRoute from "./shared/components/ProtectedRoute";
+import { AuthProvider } from "./features/auth/context/AuthContext";
 
 function App() {
 	return (
-		<DungeonProvider>
-			<MarketProvider>
-				<div className={styles["app-container"]}>
-					<Header title="DUNGEON BATTLE" />
-					<div className={styles["route-content"]}>
-						<Routes>
-							<Route path="/about" element={<AboutPage />} />
-							<Route
-								path="/register"
-								element={<RegisterPage />}
-							/>
-							<Route path="/login" element={<LoginPage />} />
+		<AuthProvider>
+			<DungeonProvider>
+				<MarketProvider>
+					<div className={styles["app-container"]}>
+						<Header title="DUNGEON BATTLE" />
+						<div className={styles["route-content"]}>
+							<Routes>
+								<Route
+									path="/"
+									element={
+										<ProtectedRoute>
+											<HomePage />
+										</ProtectedRoute>
+									}
+								/>
+								<Route path="/about" element={<AboutPage />} />
+								<Route
+									path="/register"
+									element={<RegisterPage />}
+								/>
+								<Route path="/login" element={<LoginPage />} />
 
-							<Route
-								path="/market"
-								element={
-									<ProtectedRoute>
-										<MarketplacePage />
-									</ProtectedRoute>
-								}
-							/>
+								<Route
+									path="/market"
+									element={
+										<ProtectedRoute>
+											<MarketplacePage />
+										</ProtectedRoute>
+									}
+								/>
 
-							<Route
-								path="/dungeon"
-								element={
-									<ProtectedRoute>
-										<DungeonPage />
-									</ProtectedRoute>
-								}
-							/>
-						</Routes>
-					</div>
-					<Footer />
-					<Toaster
-						toasterId="main"
-						position="bottom-right"
-						toastOptions={{
-							duration: 4000,
-							style: {
-								background: "#333",
-								color: "#fff",
-								borderRadius: "10px",
-								padding: "12px 20px",
-								fontSize: "16px",
-								maxWidth: "400px",
-							},
-							error: {
+								<Route
+									path="/dungeon"
+									element={
+										<ProtectedRoute>
+											<DungeonPage />
+										</ProtectedRoute>
+									}
+								/>
+							</Routes>
+						</div>
+						<Footer />
+						<Toaster
+							toasterId="main"
+							position="bottom-right"
+							toastOptions={{
+								duration: 4000,
 								style: {
-									background: "#ef4444",
-									color: "white",
+									background: "#333",
+									color: "#fff",
+									borderRadius: "10px",
+									padding: "12px 20px",
+									fontSize: "16px",
+									maxWidth: "400px",
 								},
-								iconTheme: {
-									primary: "white",
-									secondary: "#ef4444",
+								error: {
+									style: {
+										background: "#ef4444",
+										color: "white",
+									},
+									iconTheme: {
+										primary: "white",
+										secondary: "#ef4444",
+									},
 								},
-							},
-						}}
-					/>
-				</div>
-			</MarketProvider>
-		</DungeonProvider>
+							}}
+						/>
+					</div>
+				</MarketProvider>
+			</DungeonProvider>
+		</AuthProvider>
 	);
 }
 
--- frontend/src/features/auth/context/AuthContext.tsx
+++ frontend/src/features/auth/context/AuthContext.tsx
@@ -1,123 +1,128 @@
 import {
-  createContext,
-  useReducer,
-  useEffect,
-  ReactNode,
-  Dispatch,
+	createContext,
+	useReducer,
+	useEffect,
+	type ReactNode,
+	type Dispatch,
+	useState,
 } from "react";
 
 import {
-  AuthReducer,
-  initialState,
-  type AuthState,
-  type Action,
+	AuthReducer,
+	initialState,
+	type AuthState,
+	type Action,
 } from "../reducer/authReducer";
 
 import { loginUser, registerUser } from "../api/authApi";
 
 type AuthContextType = {
-  state: AuthState;
-  dispatch: Dispatch<Action>;
-  login: (email: string, password: string) => Promise<void>;
-  register: (name: string, email: string, password: string) => Promise<void>;
-  logout: () => void;
+	state: AuthState;
+	dispatch: Dispatch<Action>;
+	login: (email: string, password: string) => Promise<void>;
+	register: (name: string, email: string, password: string) => Promise<void>;
+	logout: () => void;
 };
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(AuthReducer, initialState);
+	const [isInitialized, setIsInitialized] = useState(false);
+	const [state, dispatch] = useReducer(AuthReducer, initialState);
 
-const register = async (name: string, email: string, password: string) => {
-    try {
-      dispatch({ type: "LOGIN_START" });
+	const register = async (name: string, email: string, password: string) => {
+		try {
+			dispatch({ type: "LOGIN_START" });
 
-      const data = await registerUser(name, email, password);
+			const data = await registerUser(name, email, password);
 
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload: {
-          user: data.user,
-          token: data.token,
-        },
-      });
+			dispatch({
+				type: "LOGIN_SUCCESS",
+				payload: {
+					user: data.user,
+					token: data.token,
+				},
+			});
 
-      localStorage.setItem("token", data.token);
-    } catch (error: any) {
-      const message =
-        error?.response?.data?.message ||
-        error.message ||
-        "Register failed";
+			localStorage.setItem("token", data.token);
+		} catch (error: any) {
+			const message =
+				error?.response?.data?.message ||
+				error.message ||
+				"Register failed";
 
-        dispatch({
-        type: "LOGIN_ERROR",
-        payload: message,
-      });
-    }
-  };
+			dispatch({
+				type: "LOGIN_ERROR",
+				payload: message,
+			});
+		}
+	};
 
-   const login = async (email: string, password: string) => {
-    try {
-      dispatch({ type: "LOGIN_START" });
+	const login = async (email: string, password: string) => {
+		try {
+			dispatch({ type: "LOGIN_START" });
 
-      const data = await loginUser(email, password);
+			const data = await loginUser(email, password);
 
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload: {
-          user: data.user,
-          token: data.token,
-        },
-      });
+			dispatch({
+				type: "LOGIN_SUCCESS",
+				payload: {
+					user: data.user,
+					token: data.token,
+				},
+			});
 
-      localStorage.setItem("token", data.token);
-    } catch (error: any) {
-      const message =
-        error?.response?.data?.message ||
-        error.message ||
-        "Login failed";
+			localStorage.setItem("token", data.token);
+		} catch (error: any) {
+			const message =
+				error?.response?.data?.message ||
+				error.message ||
+				"Login failed";
 
-        dispatch({
-        type: "LOGIN_ERROR",
-        payload: message,
-      });
-    }
-  };
+			dispatch({
+				type: "LOGIN_ERROR",
+				payload: message,
+			});
+		}
+	};
 
-  
-  
-  const logout = () => {
-    localStorage.removeItem("token");
+	const logout = () => {
+		localStorage.removeItem("token");
 
-    dispatch({ type: "LOGOUT" });
-  };
+		dispatch({ type: "LOGOUT" });
+	};
 
+	useEffect(() => {
+		const token = localStorage.getItem("token");
 
-   useEffect(() => {
-    const token = localStorage.getItem("token");
+		if (token) {
+			dispatch({
+				type: "LOGIN_SUCCESS",
+				payload: {
+					user: null,
+					token,
+				},
+			});
+		}
 
-    if (token) {
-      dispatch({
-        type: "LOGIN_SUCCESS",
-        payload: {
-          user: null,
-          token,
-        },
-      });
-    }
-  }, []);
+		setIsInitialized(true);
+	}, []);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        dispatch,
-        register,
-        login,
-        logout,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	if (!isInitialized) {
+		return <></>;
+	}
+
+	return (
+		<AuthContext.Provider
+			value={{
+				state,
+				dispatch,
+				register,
+				login,
+				logout,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
